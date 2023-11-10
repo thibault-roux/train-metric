ac4945faea64065dbc8949bfc22b9dc36871e527
thibault-roux <thibault.roux1@laposte.net>
2023-11-10 18:37:22 +0100

    Read HATS

--- train.py
+++ train.py
@@ -3,15 +3,30 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def semdist(ref, hyp, model):
-    model = memory
     ref_projection = model.encode(ref).reshape(1, -1)
     hyp_projection = model.encode(hyp).reshape(1, -1)
     score = cosine_similarity(ref_projection, hyp_projection)[0][0]
     return (1-score)*100 # lower is better
 
+def read_hats():
+    # dataset = [{"reference": ref, "hypA": hypA, "nbrA": nbrA, "hypB": hypB, "nbrB": nbrB}, ...]
+    dataset = []
+    with open("datasets/hats.txt", "r", encoding="utf8") as file:
+        next(file)
+        for line in file:
+            line = line[:-1].split("\t")
+            dictionary = dict()
+            dictionary["ref"] = line[0]
+            dictionary["hypA"] = line[1]
+            dictionary["nbrA"] = int(line[2])
+            dictionary["hypB"] = line[3]
+            dictionary["nbrB"] = int(line[4])
+            dataset.append(dictionary)
+    return dataset
+
 if __name__ == '__main__':
-    semdist_model = SentenceTransformer('dangvantuan/sentence-camembert-large')
+    model = SentenceTransformer('dangvantuan/sentence-camembert-large')
 
     ref = "Je suis un homme"
     hyp = "Je suis une femme"
-    print(semdist(ref, hyp, semdist_model))
+    print(semdist(ref, hyp, model))
