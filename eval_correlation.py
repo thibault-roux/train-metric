3d51d4e911991513d66a930c691da79ec2cf486c
thibault-roux <thibault.roux1@laposte.net>
2024-03-11 14:15:11 +0100

    Basis for correlation calculations

---
+++ eval_correlation.py
@@ -0,0 +1,83 @@
+import os
+
+
+# each metric compute scores for each pair of hypotheses and reference from HATS
+# then it compute a correlation between all these metrics
+
+
+
+def read_dataset(dataname):
+    # dataset = [{"reference": ref, "hypA": hypA, "nbrA": nbrA, "hypB": hypB, "nbrB": nbrB}, ...]
+    dataset = []
+    with open("datasets/" + dataname, "r", encoding="utf8") as file:
+        next(file)
+        for line in file:
+            line = line[:-1].split("\t")
+            dictionary = dict()
+            dictionary["reference"] = line[0]
+            dictionary["hypA"] = line[1]
+            dictionary["nbrA"] = int(line[2])
+            dictionary["hypB"] = line[3]
+            dictionary["nbrB"] = int(line[4])
+            dataset.append(dictionary)
+    return dataset
+
+
+def save_scores(metric, name, hats):
+    scores = dict()
+    for d in hats:
+        ref = d["reference"]
+        hypA = d["hypA"]
+        hypB = d["hypB"]
+        scoreA = metric(ref, hypA)
+        scoreB = metric(ref, hypB)
+        scores["reference", "hypA"] = scoreA
+        scores["reference", "hypB"] = scoreB
+    with open("results/scores/" + name + ".txt", "w", "utf8") as f:
+        for refhyp, hyp in scores.items():
+            f.write(refhyp[0] + "\t" + refhyp[1] + "\t" + str(hyp) + "\n")
+    return scores
+
+def get_scores(metric, name, hats):
+    # check if file exists
+    if not os.path.isfile("results/scores/" + name + ".txt"):
+        print("Computing scores for", name, "...")
+        return save_scores(metric, name, hats)
+    else:
+        print("Reading scores for", name, "...")
+        scores = dict()
+        with open("results/scores/" + name + ".txt", "r", "utf8") as f:
+            for line in f:
+                line = line[:-1].split("\t")
+                scores[line[0], line[1]] = float(line[2])
+        return scores
+
+
+def semdist(ref, hyp, memory):
+    model = memory    
+    ref_projection = model.encode(ref).reshape(1, -1)
+    hyp_projection = model.encode(hyp).reshape(1, -1)
+    score = cosine_similarity(ref_projection, hyp_projection)[0][0]
+    score = (1-score)*100 # lower is better
+    return score
+
+
+def wer(ref, hyp, memory):
+    return jiwer.wer(ref, hyp)
+
+def cer(ref, hyp, memory):
+    return jiwer.cer(ref, hyp)
+
+def phoner(ref, hyp, memory):
+    ep = memory
+    ref_phon = ep.transliterate(ref)
+    hyp_phon = ep.transliterate(hyp)
+    return jiwer.cer(ref_phon, hyp_phon)
+
+
+
+if __name__ == "__main__":
+    hats = read_dataset("hats.txt")
+    names = ["wer", "semdist", "cer", "phoner"]
+    metric = [wer, semdist, cer, phoner]
+
