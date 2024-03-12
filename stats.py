e32183e2f2565e92dc4ca2f7496a3e3b0b8d93de
thibault-roux <thibault.roux1@laposte.net>
2024-03-12 15:49:30 +0100

    Compute accordances with human perception

--- stats.py
+++ stats.py
@@ -33,6 +33,30 @@ def read_dataset(dataname):
             dataset.append(dictionary)
     return dataset
 
+def accordance_human_perception(hats, metricnames):
+    # compute the number of times the human perception is in accordance with the scores
+    accordances = [] # contains a list of booleans for each triplet
+    for triplet in hats:
+        ref = triplet["reference"]
+        hypA = triplet["hypA"]
+        hypB = triplet["hypB"]
+        nbrA = triplet["nbrA"]
+        nbrB = triplet["nbrB"]
+        accord = [0] * len(metricnames)
+        for i, metricname in enumerate(metricnames):
+            if scores[ref, hypA] < scores[ref, hypB]:
+                if nbrA < nbrB:
+                    accord[i] = 1
+            elif scores[ref, hypA] > scores[ref, hypB]:
+                if nbrA > nbrB:
+                    accord[i] = 1
+            else:
+                if nbrA == nbrB:
+                    accord[i] = 1
+        accordances.append(accord)
+    return accordances
+        
+
 if __name__ == "__main__":
     metricnames = ["semdist", "wer", "cer", "phoner"]
     all_scores = dict()
@@ -40,3 +64,7 @@ if __name__ == "__main__":
         all_scores[metricname] = get_scores(metricname)
 
     hats = read_dataset("hats.txt")
+
+    accordances = accordance_human_perception(hats, metricnames)
+    for accord in accordances:
+        print(accord)
