1c1b85418ee0ba921abfe0ca3a938356bc8e79d8
thibault-roux <thibault.roux1@laposte.net>
2024-06-05 18:22:13 +0200

    Infer on data set

--- llm/5llm.py
+++ llm/5llm.py
@@ -1,11 +1,12 @@
 import os
 from openai import OpenAI
 import pickle
+import progressbar
 
 
 # source ~/.zshrc
 
-def chat(ref, hypA, hypB):
+def chat(ref, hypA, hypB, i):
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
@@ -15,22 +16,66 @@ def chat(ref, hypA, hypB):
         {"role": "user", "content": "Référence : " + ref + "\nHypothèse A : " + hypA + "\nHypothèse B : " + hypB},
     ]
     )
-    pickle.dump(response, open("response2.pkl", "wb"))
+    pickle.dump(response, open("pickle/" + str(i) + ".pkl", "wb"))
 
 
-ihavemoney = True
+def read_hats(namefile):
+    dataset = []
+    with open("../datasets/" + namefile + ".txt", "r", encoding="utf8") as file:
+        next(file)
+        for line in file:
+            dictionary = dict()
+            line = line[:-1].split("\t")
+            dictionary["reference"] = line[0]
+            dictionary["hypA"] = line[1]
+            nbrA = int(line[2])
+            dictionary["hypB"] = line[3]
+            nbrB = int(line[4])
+            if nbrA > nbrB:
+                dictionary["best"] = "A"
+            elif nbrA < nbrB:
+                dictionary["best"] = "B"
+            else:
+                continue
+            dataset.append(dictionary)
+    return dataset
+
+
+ihavemoney = False
 if ihavemoney:
     client = OpenAI(
         api_key=os.getenv("OPENAI_API_KEY"),
     )
-    ref = "c' est à lui même"
-    hypA = "êtes à lui même"
-    hypB = "c' est euh à lui-même"
-    chat(ref, hypA, hypB)
 
-response = pickle.load(open("response2.pkl", "rb"))
+def infer(namefile):
+    txt = "reference\thypA\tnbrA\thypB\tnbrB\n"
+    for i, data in enumerate(dataset):
+        chat(data["reference"], data["hypA"], data["hypB"], i)
+        print(i)
+        answer = response.choices[0].message.content
+        if answer == "A":
+            txt += data["reference"] + "\t" + data["hypA"] + "\t7\t" + data["hypB"] + "\t0\n"
+        elif answer != "B":
+            txt += data["reference"] + "\t" + data["hypA"] + "\t0\t" + data["hypB"] + "\t7\n"
+        else:
+            print("Weird output:", answer)
+
+def eval(namefile):
+    dataset = read_hats(namefile)
+    correct = 0
+    incorrect = 0
+
+        if response.choices[0].message.content == data["best"]:
+            correct += 1
+        else:
+            incorrect += 1
+            if response.choices[0].message.content != "A" or response.choices[0].message.content != "B":
+                print("Weird output:", response.choices[0].message.content)
+        
 
 
-print(response)
-print()
-print(response.choices[0].message.content)
+if __name__ == "__main__":
+    # response = pickle.load(open("response2.pkl", "rb"))
+    # print(response.choices[0].message.content)
+
+    namefile = "hats_train"
