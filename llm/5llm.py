1325b81691702eb033e28119d5b3e48d077a0201
thibault-roux <thibault.roux1@laposte.net>
2024-06-05 18:08:31 +0200

    Working method

--- llm/5llm.py
+++ llm/5llm.py
@@ -2,26 +2,33 @@ import os
 from openai import OpenAI
 import pickle
 
-ihavemoney = True
 
-if ihavemoney:
-    client = OpenAI(
-        api_key = os.environ.get("OPENAI_API_KEY"),
-    )
+# source ~/.zshrc
 
-
+def chat(ref, hypA, hypB):
     response = client.chat.completions.create(
     model="gpt-3.5-turbo",
     messages=[
         {"role": "system", "content": "Tu écris seulement 'A' ou 'B' pour choisir la meilleure transcription."},
         {"role": "user", "content": "Référence : Mon radiateur est en panne\nHypothèse A : Mon mon radiateur est en panne\nHypothèse B : Mon radieux pend"},
         {"role": "assistant", "content": "A"},
+        {"role": "user", "content": "Référence : " + ref + "\nHypothèse A : " + hypA + "\nHypothèse B : " + hypB},
     ]
     )
+    pickle.dump(response, open("response2.pkl", "wb"))
 
-    pickle.dump(response, open("response.pkl", "wb"))
-else:
-    response = pickle.load(open("response.pkl", "rb"))
+
+ihavemoney = True
+if ihavemoney:
+    client = OpenAI(
+        api_key=os.getenv("OPENAI_API_KEY"),
+    )
+    ref = "c' est à lui même"
+    hypA = "êtes à lui même"
+    hypB = "c' est euh à lui-même"
+    chat(ref, hypA, hypB)
+
+response = pickle.load(open("response2.pkl", "rb"))
 
 
 print(response)
