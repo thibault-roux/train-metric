5b227837c4cf4f752bcaa7a0bfb02be174154d4f
thibault-roux <thibault.roux1@laposte.net>
2024-06-05 16:46:20 +0200

    Add correct prompting

--- llm/4llm.py
+++ llm/4llm.py
@@ -8,21 +8,24 @@ Hypothèse A : Mon mon radiateur est en panne
 Hypothèse B : Mon radieux pend
 Choix : <s>A
 </s>Référénce : Le chat est sur le tapis
-Hypothèse A : Le chien est sur le tapis
-Hypothèse B : Le pain peint le papi
-Choix : <s>A
+Hypothèse A : Le pain peint le papi
+Hypothèse B : Le chien est sur le tapis
+Choix : <s>B
 </s>Référence : Je vais voir mamie
 Hypothèse A : Je monte marre de pire
 Hypothèse B : Je vais voir mami
-Choix : <s>"""
+Choix : <s>B"""
 
+def add2prompt(prompt, ref, hypA, hypB):
+    prompt += "\n</s>Référence : " + ref + "\nHypothèse A : " + hypA + "\nHypothèse B : " + hypB  + "\nChoix : <s>"
+    return prompt
 
-result = model(prompt, max_new_tokens=1)
-
-print(result)
 
 while True:
     print()
-    inp = input("Saisir votre texte : ")
+    ref = input("Saisir la référence: ")
+    hypA = input("Saisir l'hypothèse A: ")
+    hypB = input("Saisir l'hypothèse B: ")
+    txt = add2prompt(prompt, ref, hypA, hypB)
     result = model(txt, max_new_tokens=1)
     print(result)
