cac5d07ffd5bb313fabb10082afe70dab83d9c8e
thibault-roux <thibault.roux1@laposte.net>
2024-06-05 16:35:35 +0200

    Working method

--- llm/4llm.py
+++ llm/4llm.py
@@ -2,14 +2,27 @@ from transformers import pipeline
 
 model = pipeline("text-generation", "cmarkea/bloomz-560m-sft-chat")
 
-# txt = "</s>C'est quoi le deep learning ?<s>"
-txt = "</s>Selon la référence, écris juste A ou B pour sélectionner la meilleure transcription parmi les deux hypothèses suivantes :\nRéférence : Mon radiateur est en panne\tHypothèse A : Mon mon radiateur est en panne\tHypothèse B : Mon radieux pend\t</s>"
-result = model(txt, max_new_tokens=512)
+prompt = """</s>Selon la référence, écris juste A ou B pour sélectionner la meilleure transcription parmi les deux hypothèses suivantes :
+Référence : Mon radiateur est en panne
+Hypothèse A : Mon mon radiateur est en panne
+Hypothèse B : Mon radieux pend
+Choix : <s>A
+</s>Référénce : Le chat est sur le tapis
+Hypothèse A : Le chien est sur le tapis
+Hypothèse B : Le pain peint le papi
+Choix : <s>A
+</s>Référence : Je vais voir mamie
+Hypothèse A : Je monte marre de pire
+Hypothèse B : Je vais voir mami
+Choix : <s>"""
+
+
+result = model(prompt, max_new_tokens=1)
 
 print(result)
 
 while True:
     print()
-    txt = input("Saisir votre texte : ")
-    result = model(txt, max_new_tokens=512)
+    inp = input("Saisir votre texte : ")
+    result = model(txt, max_new_tokens=1)
     print(result)
