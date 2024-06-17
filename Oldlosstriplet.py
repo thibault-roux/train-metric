4360a25450361e313b4246ddb5a4c958ac2a02ee
thibault-roux <thibault.roux1@laposte.net>
2024-06-18 00:03:51 +0200

    add filter to evaluation

--- Oldlosstriplet.py
+++ Oldlosstriplet.py
@@ -38,7 +38,7 @@ def semdist(ref, hyp, model):
     score = cosine_similarity(ref_projection, hyp_projection)[0][0]
     return (1-score)*100 # lower is better
 
-def evaluate(model, namefile):
+def evaluate(model, namefile, filter=0):
     print("Evaluating...")
     correct = 0
     incorrect = 0
@@ -51,6 +51,9 @@ def evaluate(model, namefile):
             hypB = line[3]
             nbrA = int(line[2])
             nbrB = int(line[4])
+            agreement = max(nbrA, nbrB) / (nbrA + nbrB)
+            if agreement <= filter:
+                continue
             scoreA = semdist(reference, hypA, model)
             scoreB = semdist(reference, hypB, model)
             if scoreA < scoreB and nbrA > nbrB:
@@ -67,17 +70,18 @@ def evaluate(model, namefile):
 
 
 if __name__ == "__main__":
-    model = SentenceTransformer('dangvantuan/sentence-camembert-base')
-    train_dataloader = get_dataloader("hats_extended_chatgpt") # hats_train, hats_train_best, hats_extended, hats_extended_chatgpt
+    # model = SentenceTransformer('dangvantuan/sentence-camembert-base')
+    model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+    train_dataloader = get_dataloader("hats_train_plus_gpt") # hats_train, hats_train_best, hats_extended, hats_extended_chatgpt, hats_train_plus_gpt
     train_loss = sentence_transformers.losses.TripletLoss(model=model)
 
     # evaluate(model, "hats_test") # hats_test, hats_test_best # 0.7162698412698413
     
     # Tune the model
-    print("Fine tuning...")
-    model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=3, warmup_steps=10) #, optimizer_params={'lr': 2e-6}) # train_best, epochs=3, warmup_steps=10 worked well: 72.61
+    # print("Fine tuning...")
+    # model.fit(train_objectives=[(train_dataloader, train_loss)], epochs=3, warmup_steps=10) #, optimizer_params={'lr': 2e-6}) # train_best, epochs=3, warmup_steps=10 worked well: 72.61
     # save in models/losstriplet
     # model.save("models/losstriplet2")
 
     # Test the model
-    evaluate(model, "hats_test") # hats_test, hats_test_best
+    evaluate(model, "hats_test", 1) # hats_test, hats_test_best
