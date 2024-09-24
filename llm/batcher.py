4a91ea24a504dca70817d586e87edf238a34da67
thibault-roux <thibault.roux1@laposte.net>
2024-09-24 16:51:25 +0200

    Good format

--- llm/batcher.py
+++ llm/batcher.py
@@ -1,5 +1,6 @@
 from openai import OpenAI
 import pickle
+import json
 
 
 # step 1 : save the file in a json
@@ -19,10 +20,10 @@ if __name__ == "__main__":
     letscreate = False
     letsprintbatchobject = False
     letscheckstatus = False
-    letsretrieve = True
+    letsretrieve = False
+    letsgoodformat = True
 
-
-    inputname = "new_dataset_subset" # batchinput
+    inputname = "hats-5k" # "new_dataset" # new_dataset_subset # batchinput
 
 
     if letsupload:
@@ -47,12 +48,12 @@ if __name__ == "__main__":
         )
 
         # save batch object
-        with open("pickle/batchobject.pkl", "wb") as f:
+        with open("pickle/" + inputname + "_batchobject.pkl", "wb") as f:
             pickle.dump(batch_object, f)
 
     if letsprintbatchobject:
         # print batch object
-        with open("pickle/batchobject.pkl", "rb") as f:
+        with open("pickle/" + inputname + "_batchobject.pkl", "rb") as f:
             batch_object = pickle.load(f)
             # print(batch_object)
             print(batch_object.id)
@@ -61,17 +62,37 @@ if __name__ == "__main__":
     if letscheckstatus:
         # check status
         # get batch object
-        with open("pickle/batchobject.pkl", "rb") as f:
+        with open("pickle/" + inputname + "_batchobject.pkl", "rb") as f:
             batch_object = pickle.load(f)
             # print(batch_object)
         tmp = client.batches.retrieve(batch_object.id)
         print(tmp)
-        print(tmp.output_file_id)
+        print("The status of the batch is: " + tmp.status + "!")
 
     if letsretrieve:
         # retrieve the results
-        with open("pickle/batchobject.pkl", "rb") as f:
+        with open("pickle/" + inputname + "_batchobject.pkl", "rb") as f:
             batch_object = pickle.load(f)
         tmp = client.batches.retrieve(batch_object.id)
         file_response = client.files.content(tmp.output_file_id)
         print(file_response.text)
+        # save pickle
+        with open("pickle/" + inputname + "_response.pkl", "wb") as f:
+            pickle.dump(file_response, f)
+
+    if letsgoodformat:
+        # read pickle
+        with open("pickle/" + inputname + "_response.pkl", "rb") as f:
+            file_response = pickle.load(f)
+            # print(file_response)
+            
+            txt = ""
+            for line in file_response.text.split("\n"):
+                # convert line to json
+                try:
+                    line = json.loads(line)
+                    txt += line["custom_id"] + "\t" + line["response"]["body"]["choices"][0]["message"]["content"] + "\n"
+                except:
+                    print(line)
+        with open("batch/" + inputname + "_results.txt", "w") as f:
+            f.write(txt)
