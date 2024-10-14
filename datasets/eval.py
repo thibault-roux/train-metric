225f8c7646f6f00ce49dd2f22458c5fc73a909c9
thibault-roux <thibault.roux1@laposte.net>
2024-10-14 02:03:15 +0200

    load original hats for filter

--- datasets/eval.py
+++ datasets/eval.py
@@ -11,6 +11,21 @@ def load(namefile):
             data.append(line)
     return data
 
+def load_OG():
+    with open("./hats.txt", "r") as f:
+        next(f)
+        agreement = []
+        for line in f:
+            line = line.strip().split("\t")
+            # ref = line[0]
+            # hypA = line[1]
+            # hypB = line[3]
+            nbrA = line[2]
+            nbrB = line[4]
+            agree = max(nbrA, nbrB)/(nbrA + nbrB)
+            agreement.append(agree)
+    return agreement
+
 def compare(namefile1, namefile2):
     data1 = load(namefile1)
     data2 = load(namefile2)
